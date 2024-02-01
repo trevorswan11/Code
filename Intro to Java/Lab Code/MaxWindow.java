0ff5752194c98e09af96617d5b4281b7bd63dcd2
Trevor Swan <tcs94@case.edu>
2024-02-01 16:44:02 -0500

    Fixed personal lab 3 code

--- Intro to Java/Lab Code/MaxWindow.java
+++ Intro to Java/Lab Code/MaxWindow.java
@@ -1,48 +1,41 @@
-/* CSDS132 Lab 3
- Trevor Swan
- Curtis Li
- Simon Eskin */
-
+//Names: Simon Eskin, Trevor Swan, Curtis Li
 import javax.swing.JFrame;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-public class MaxWindow extends JFrame{
-    public static void main(String[] args){
-        MaxWindow m1 = new MaxWindow();
-        m1.setVisible(true);
-        m1.snapToTop();
-        m1.snapToLeft();
-        boolean heightMaximized;
-        m1.maximizeHeight(true);
-        m1.maximizeWidth();
-        int originalHeight = 0;
+
+public class MaxWindow extends JFrame {
+  private boolean heightMaximized = false;
+  private int originalHeight = 0;
+  private int originalY = 0;
+  //this snaps the window to the left of the screen
+  public void snapToLeft(){
+    setLocation(0,getY());
+  }
+  //this sends the window to the top of the screen
+  public void snapToTop(){
+    setLocation(getX(),0); 
+  }
+  public void maximizeHeight(boolean x){
+    Toolkit kit = Toolkit.getDefaultToolkit();
+    Dimension d = kit.getScreenSize();
+    if(x == true&&heightMaximized == false){
+      originalHeight = getHeight();
+      originalY = getY();
+      setLocation(getX(),0);
+      setSize(getWidth(),(int)d.getHeight());
+      heightMaximized = true;
     }
-    // this snaps the window to the top of the screen
-    public void snapToTop() {
-        setLocation(getX(),0);
+    else if(x == false&&heightMaximized == true){
+      setSize(getWidth(),originalHeight);
+      setLocation(getX(),originalY);
+      heightMaximized = false;
     }
-    // this snaps the window to the left of the screen
-    public void snapToLeft() {
-        setLocation(0, getY());
-    }
-    public void maximizeHeight(boolean x) {
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension d = kit.getScreenSize();
-        if (x == true && heightMaximized == false) {
-            int originalHeight = (int)getHeight();
-            setLocation(getX(), 0);
-            setSize(getWidth(),(int)d.getHeight());
-            heightMaximized = true;
-        } else if (x == false && heightMaximized == true) {
-            setSize(getWidth(), originalHeight);
-            heightMaximized = false;
-        }
-    }
-    public void maximizeWidth() {
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension d = kit.getScreenSize();
-        snapToLeft();
-        setSize((int)d.getWidth(),getHeight());
-    }
+  }
+  public void maximizeWidth(boolean x){
+    Toolkit kit = Toolkit.getDefaultToolkit();
+    Dimension d = kit.getScreenSize();
+    setLocation(0,getY());
+    setSize((int)d.getWidth(),getHeight());
+  }
 }
