caec4e21d90d03ada2a8cf71fe997b66bb123d62
Trevor Swan <tcs94@case.edu>
2024-02-02 14:27:47 -0500

    Fixed again

--- Intro to Java/Lecture Code/Die.java
+++ Intro to Java/Lecture Code/Die.java
@@ -1,3 +1,16 @@
-public class Die {
-    
-}
+// This class represents a die game
+public class Die extends Object{
+    // A field to represent the current vlaue
+  private int currentValue = 1;
+  public static void main(String[] args){
+      
+  }
+  //  A method to access the current value
+  public int getValue(){
+  return currentValue;
+  }
+  //  A method to change the current value with input
+  public void setValue(int value){
+  currentValue = value;
+  }
+  }
