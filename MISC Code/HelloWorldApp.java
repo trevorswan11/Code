edd3d6e2a734b943597e40c543dd9e8d15e000fb
Trevor Swan <tcs94@case.edu>
2024-02-03 02:35:25 -0500

    JFrame printing test

---
+++ MISC Code/HelloWorldApp.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World Example");
+        JPanel panel = new JPanel(); // Create a JPanel
+        JLabel label = new JLabel("Hello World!");
+
+        panel.add(label); // Add the label to the panel
+        frame.add(panel); // Add the panel to the frame
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
