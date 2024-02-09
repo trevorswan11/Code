f05e5f434f1b930028196545e2d1e258a0d340ec
Trevor Swan <tcs94@case.edu>
2024-02-09 15:06:06 -0500

    folder organization and updated some project code/tests

---
+++ CSDS132/Lecture Code/Employee/HourlyEmployee.java
@@ -0,0 +1,11 @@
+public class HourlyEmployee extends Employee {
+    // private ;
+
+    public HourlyEmployee(String name) {
+        super(name);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
