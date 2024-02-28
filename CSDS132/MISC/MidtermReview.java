7201ccd483de44a13e790bc83b74c29a444d1933
Trevor Swan <tcs94@case.edu>
2024-02-28 16:12:29 -0500

    Started thinking about midterm review
    Created a file for practice code. It extends a class called TypeChecker that overloads a method named type based on the input and returns the input after displaying a message about the type.
    
    2/28/24 @ 4:12pm

---
+++ CSDS132/MISC/MidtermReview.java
@@ -0,0 +1,5 @@
+public class MidtermReview extends TypeChecker {
+    public static void main(String[] args) {
+        System.out.println(type(new Object() == new Object()));
+    }
+}
---
+++ CSDS132/MISC/TypeChecker.java
@@ -0,0 +1,50 @@
+/** This class overloads a method I wrote to determine what type an input is */
+public class TypeChecker {
+    // Checks if input is an int
+    public static int type(int input) {
+        System.out.println(input + " is an int.");
+        return input;
+    }
+
+    // Checks if the input if a byte
+    public static byte type(byte input) {
+        System.out.println(input + " is a byte.");
+        return input;
+    }
+
+    // Checks if the input is a float
+    public static float type(float input) {
+        System.out.println(input + " is a float.");
+        return input;
+    }
+
+    // Checks if the input is a double
+    public static double type(double input) {
+        System.out.println(input + " is a double.");
+        return input;
+    }
+
+    // Checks if the input is a String
+    public static String type(String input) {
+        System.out.println(input + " is a String.");
+        return input;
+    }
+
+    // Checks if the input is a boolean
+    public static boolean type(boolean input) {
+        System.out.println(input + " is an int.");
+        return input;
+    }
+
+    // Checks if the input is a char
+    public static char type(char input) {
+        System.out.println(input + " is a char.");
+        return input;
+    }
+
+    // Checks if the input is an Object
+    public static Object type(Object input) {
+        System.out.println(input + " is an object.");
+        return input;
+    }
+}
