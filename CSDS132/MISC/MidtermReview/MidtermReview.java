ff18664f7a2d6bcb046b45008973a5dad60fd609
Trevor Swan <tcs94@case.edu>
2024-03-05 12:20:40 -0500

    COmmmit some random changes

--- CSDS132/MISC/MidtermReview/MidtermReview.java
+++ CSDS132/MISC/MidtermReview/MidtermReview.java
@@ -1,5 +1,5 @@
 public class MidtermReview {
     public static void main(String[] args) {
-        System.out.println(TypeChecker.type((char)0));
+        System.out.println(TypeChecker.type((byte)129));
     }
 }
--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,6 @@
-print("hello world")
+def isPalendrome(a_string):
+    stripped = a_string.lower().replace(' ','');
+    return stripped == stripped[::-1]
+
+input_string = 'was it a car or a cat I saw'
+print(isPalendrome(input_string))
