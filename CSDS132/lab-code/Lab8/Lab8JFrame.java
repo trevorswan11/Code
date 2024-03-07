6d9a0315342ca2ae3ee330cb1286fd4cdb6fcb31
Trevor Swan <tcs94@case.edu>
2024-03-07 15:25:21 -0500

    Finished Lab8

--- CSDS132/lab-code/Lab8/Lab8JFrame.java
+++ CSDS132/lab-code/Lab8/Lab8JFrame.java
@@ -1,11 +1,14 @@
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import java.io.*;
+
 /**
 * A window with a text area that works as a simple text editor.
-* @author <em>Your name goes here</em>
-* @author <em>Your partner's name goes here</em>
-*/
+* @author Trevor Swan 
+* @author Simon Eskin
+* @author Curtis Li
+* */
+
 public class Lab8JFrame extends JFrame {
     /** Text area, in the center. */
     private JTextArea textArea;
@@ -28,4 +31,43 @@ public class Lab8JFrame extends JFrame {
     public JTextArea getTextArea() {
         return textArea;
     }
+	
+	/** 
+    * Displays the contents of a file to the center text area.
+    * @param fileName  the file to be displayed
+    * @throws IOException if there is a problem reading from input file.
+    */
+	public void fileDisplay(String fileName) throws IOException {
+		getTextArea().setText("");
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		// initialize a character and loop through the file until -1 is returned
+		int currentChar;
+		while ((currentChar = reader.read()) != -1) {
+			String firstChar = Character.toString((char)currentChar);
+			getTextArea().append(firstChar);
+			getTextArea().append(reader.readLine());
+			getTextArea().append("\n");
+		}
+
+		// pack the JFrame and close reader
+		this.pack();
+		reader.close();
+	}
+
+	/** Does not work as intended but should add text from JFrame to specified file
+	 *
+	 * @param fileDestination file path
+	 * @return void
+	 */
+	public void saveFile(String fileDestination) throws IOException {
+		PrintStream p = new PrintStream(new FileOutputStream(fileDestination));	
+		p.print(this.getTextArea());
+	}
+
+	public static void main(String[] args) throws IOException {
+		Lab8JFrame frame = new Lab8JFrame();
+		frame.setVisible(true);
+		frame.fileDisplay("C:/Users/Trevor/OneDrive/Documents/CWRU/Code/hello.txt");
+		frame.saveFile("C:/Users/Trevor/OneDrive/Documents/CWRU/Code/hello.txt");
+	}
 }
