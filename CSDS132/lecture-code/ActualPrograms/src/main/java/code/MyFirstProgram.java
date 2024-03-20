b5e88a0101ad574e5c7367f78bc6c26c83e2a7ec
Trevor Swan <tcs94@case.edu>
2024-03-20 15:06:30 -0400

    lecture stuff

--- CSDS132/lecture-code/ActualPrograms/src/main/java/code/MyFirstProgram.java
+++ CSDS132/lecture-code/ActualPrograms/src/main/java/code/MyFirstProgram.java
@@ -4,6 +4,43 @@ package code;
 public class MyFirstProgram {
 
 	/**
+	 * This method manually calculates the integer power of a double
+	 * 
+	 * @param base The base of the expression. A double value
+	 * @param exp  The integer exponent to use
+	 * @return The result of the caluclation as a double
+	 */
+	public static double power(double base, int exp) {
+		double product = 1;
+		// If base is 0 and exp is not, return 1
+		if (base == 0 && exp != 0) {
+			return 1;
+		} else if (base == 0 && exp == 0) {
+			return Integer.MAX_VALUE;
+		}
+
+		// Check exp against 0 and declare default case
+		switch (exp) {
+			case 0:
+				return 1;
+			default:
+				if (exp < 0) {
+					for (int i = 1; i <= -exp; i++) {
+						product = product / base;
+					}
+				}
+
+				// Otherwise calculate the normal expression
+				else {
+					for (int i = 1; i <= exp; i++) {
+						product = product * base;
+					}
+				}
+				return product;
+		}
+	}
+
+	/**
 	 * The method run first by the program
 	 * 
 	 * @param args command line arguments
@@ -21,5 +58,6 @@ public class MyFirstProgram {
 		System.out.println("The command line arguments are:");
 		for (int i = 0; i < args.length; i++)
 			System.out.println(args[i]);
+		System.out.println(power(2, -3));
 	}
 }
