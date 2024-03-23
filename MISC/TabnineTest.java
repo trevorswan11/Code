d2bcf6fd856ad99253b18d4a380236a021404aea
Trevor Swan <tcs94@case.edu>
2024-03-23 13:49:26 -0400

    Downloaded Tabnine for personal use

---
+++ MISC/TabnineTest.java
@@ -0,0 +1,15 @@
+public class TabnineTest {
+    private static String TEST_STRING = "The quick brown fox jumps over the lazy dog";
+
+    public String getTestString() {
+        return TEST_STRING;
+    }
+
+    /**
+     * 
+     * @param testString
+     */
+    public void setTestString(String testString) {
+        TEST_STRING = testString;
+    }
+}
