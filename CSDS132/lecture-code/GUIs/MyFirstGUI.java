1a0007443bd53e1bde002fac8b2cd640efc8d263
Trevor Swan <tcs94@case.edu>
2024-04-03 15:08:07 -0400

    Lecture code and mvn-i on project

--- CSDS132/lecture-code/GUIs/MyFirstGUI.java
+++ CSDS132/lecture-code/GUIs/MyFirstGUI.java
@@ -1,3 +1,28 @@
-public class MyFirstGUI {
+// You should know these by heart after the fourth project
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * A GUI example from lecture.
+ */
+public class MyFirstGUI extends Application {
+    /**
+     * Starts the application.
+     */
+    @Override
+    public void start(Stage primaryStage){
+        Button button = new Button("TOUCH ME");
+        Scene scene = new Scene(button);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
+    /**
+     * Launches the application.
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
