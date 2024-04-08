75db7fff2f328dac7f2f295d9305b17a2a602d49
Trevor Swan <tcs94@case.edu>
2024-04-08 13:14:44 -0400

    Did PreLab12

--- CSDS132/lab-code/Lab12/src/main/java/prelabcode12/PreLab12.java
+++ CSDS132/lab-code/Lab12/src/main/java/prelabcode12/PreLab12.java
@@ -6,9 +6,15 @@ import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.Button;
 
+/**
+ * A JavaFX class that creates a GUI with two buttons that print 
+ * a required statement to the screen when pressed.
+ * 
+ * @author Trevor Swan
+ * @version CSDS132 - Spring 2024  
+ */
 public class PreLab12 extends Application {
     
     /**
@@ -17,6 +23,45 @@ public class PreLab12 extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
-        
+        // Initialize the two buttons
+        Button buttonOne = new Button("Button 1");
+        Button buttonTwo = new Button("Button 2");
+
+        // Event handler for buttonOne
+        buttonOne.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("You clicked Button 1");
+            }
+        });
+
+        // Event handler for buttonTwo
+        buttonTwo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("You clicked Button 2");
+            }
+        });
+
+        // Create border pane to organize the buttons
+        BorderPane pane = new BorderPane();
+        pane.setLeft(buttonOne);
+        pane.setRight(buttonTwo);
+
+        // Create a scene to contain the pane
+        Scene scene = new Scene(pane);
+
+        // Set a title to the stage and set/show it
+        primaryStage.setTitle("Pre-Lab for Lab 12");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * Launches applications with command line arguments
+     * @param args Command line input
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
