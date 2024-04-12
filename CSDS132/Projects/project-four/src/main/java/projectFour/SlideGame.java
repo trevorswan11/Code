50ba782a26f686defb714ab7086da9424c511d85
Trevor Swan <tcs94@case.edu>
2024-04-12 15:04:48 -0400

    Fixed row,col format

--- CSDS132/Projects/project-four/src/main/java/projectFour/SlideGame.java
+++ CSDS132/Projects/project-four/src/main/java/projectFour/SlideGame.java
@@ -1,11 +1,15 @@
 package projectFour;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Random;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -22,59 +26,6 @@ public class SlideGame extends Application {
 
     // An int array to store the state of the game board
     private int[][] gameBoard;
-
-    /**
-     * A helper method to parse the args of the command line to set the dimensions
-     * of the board. Takes command line arguments and directly modifies the
-     * dimensions.
-     * 
-     * @param params The command line arguments
-     */
-    private void parseArgs(Parameters params) {
-        // Variables to store the length and width
-        int length = -1;
-        int width = -1;
-
-        // Parse the command line arguments
-        if (!params.getRaw().isEmpty()) {
-            try {
-                width = Integer.parseInt(params.getRaw().get(0));
-                length = Integer.parseInt(params.getRaw().get(1));
-            } catch (Exception e) {
-                length = 4;
-                width = 4;
-            }
-        }
-
-        // Otherwise set length and width to 4
-        else {
-            length = 4;
-            width = 4;
-        }
-
-        // If the length is unreasonable, change to a rounded value
-        if (length < 4) {
-            length = 4;
-        }
-
-        // I want 10 x 10 to be the max
-        else if (length > 10) {
-            length = 10;
-        }
-
-        // If the width is unreasonable, change to a rounded value
-        if (width < 4) {
-            width = 4;
-        }
-
-        // I want 10 x 10 to be the max
-        else if (width > 10) {
-            width = 10;
-        }
-
-        // Set the board dimensions based on length and width
-        this.setBoardDim(new int[] { width, length });
-    }
 
     /**
      * A method to retrieve the boards dimensions.
@@ -113,13 +64,66 @@ public class SlideGame extends Application {
     }
 
     /**
+     * A helper method to parse the args of the command line to set the dimensions
+     * of the board. Takes command line arguments and directly modifies the
+     * dimensions.
+     * 
+     * @param params The command line arguments
+     */
+    private void parseArgs(Parameters params) {
+        // Variables to store the row and column value
+        int row = -1;
+        int col = -1;
+
+        // Parse the command line arguments
+        if (!params.getRaw().isEmpty()) {
+            try {
+                row = Integer.parseInt(params.getRaw().get(0));
+                col = Integer.parseInt(params.getRaw().get(1));
+            } catch (Exception e) {
+                col = 4;
+                row = 4;
+            }
+        }
+
+        // Otherwise set length and row to 4
+        else {
+            col = 4;
+            row = 4;
+        }
+
+        // If the length is unreasonable, change to a rounded value
+        if (col < 4) {
+            col = 4;
+        }
+
+        // I want 10 x 10 to be the max
+        else if (col > 10) {
+            col = 10;
+        }
+
+        // If the row is unreasonable, change to a rounded value
+        if (row < 4) {
+            row = 4;
+        }
+
+        // I want 10 x 10 to be the max
+        else if (row > 10) {
+            row = 10;
+        }
+
+        // Set the board dimensions based on length and width
+        this.setBoardDim(new int[] { row, col });
+    }
+
+    /**
      * A Method to set an individual value in the board game
      * 
      * @param val      The value to be placed in the array
      * @param position an array indicating the position in the game to alter as
      *                 {row, column}
      */
-    public void setPosition(int val, int[] position) {
+    private void setPosition(int val, int[] position) {
         // Create a temporary board
         int[][] gameBoard = this.getGameBoard();
 
@@ -146,46 +150,47 @@ public class SlideGame extends Application {
         this.parseArgs(getParameters());
 
         // Retrieve the board's dimensions
-        int length = this.getBoardDim()[0];
-        int width = this.getBoardDim()[1];
+        int row = this.getBoardDim()[0];
+        int col = this.getBoardDim()[1];
 
         // Create a GridPane of buttons fitting the dimensions of the board
-        Button[][] buttons = new Button[width][length];
+        Button[][] buttons = new Button[row][col];
         GridPane buttonPane = new GridPane();
-
-        // Generate random indices for the original 1
-        // int[] randomStart = this.randomIndex();
 
         // Generate the original board and set one random place to 1
         this.setGameBoard(this.emptyBoard(this.getBoardDim()));
-        this.setPosition(1, new int[] {1,1});
+        this.setPosition(1, this.randomIndex());
 
         /*
          * Creates buttons and adds them to the button pane.
-         * precondition: length and width have been determined
+         * precondition: row and col have been determined
          * postcondition: button pane has array with dimensions matching gameBoard
          */
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 // Create a button and store it in the array of buttons
                 Button button = new Button();
-                buttons[j][i] = button;
+                buttons[i][j] = button;
 
                 // Set the size and position of the button
                 button.setMinSize(50, 50);
                 button.setAlignment(Pos.CENTER);
 
                 // Set the text of the button to the text in the game board
-                String text = Integer.toString(getGameBoard()[j][i]);
+                String text = Integer.toString(this.getGameBoard()[i][j]);
                 button.setText(text.equals("0") ? "" : text);
 
                 // Add the button to the pane
-                buttonPane.add(button, i, j);
+                buttonPane.add(button, j, i);
             }
         }
 
+        // Organize the GUI with a BorderPane
+        BorderPane slideGame = new BorderPane();
+        slideGame.setCenter(buttonPane);
+
         // Create/set/show the scene
-        Scene scene = new Scene(buttonPane);
+        Scene scene = new Scene(slideGame);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Slide Game");
         primaryStage.show();
@@ -196,13 +201,13 @@ public class SlideGame extends Application {
      * 
      * @param dimensions Length x Width array to create the empty board
      */
-    public int[][] emptyBoard(int[] dimensions) {
-        // Create a array with width nested arrays
-        int[][] board = new int[dimensions[1]][];
+    private int[][] emptyBoard(int[] dimensions) {
+        // Create a array with row nested arrays
+        int[][] board = new int[dimensions[0]][];
 
         // Add length arrays of zeros to the board
-        for (int i = 0; i < dimensions[1]; i++) {
-            board[i] = new int[dimensions[0]];
+        for (int i = 0; i < dimensions[0]; i++) {
+            board[i] = new int[dimensions[1]];
         }
 
         // Return the full board
@@ -217,44 +222,31 @@ public class SlideGame extends Application {
      *         of zero-values
      * @throws NoSuchElementException when no indices fir the criteria of being zero 
      */
-    public int[] randomIndex() throws NoSuchElementException {
-        // retrieve the game board and the total number of elements in it
+    private int[] randomIndex() throws NoSuchElementException {
+        // Grab the board field
         int[][] board = this.getGameBoard();
-        int size = this.getBoardDim()[0] * this.getBoardDim()[1];
 
-        // int to store number of non-zero spaces
-        int nonZeros = 0;
+        // Create an empty array list to store potential zero indices
+        List<int[]> zeroIndices = new ArrayList<>();
 
-        // Check for a non-zero location in the board
+        // Find all indices with zero values
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                // if a non-zero is found, add to the index
-                if (board[i][j] != 0) {
-                    nonZeros++;
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 0) {
+                    zeroIndices.add(new int[]{i, j});
                 }
             }
         }
 
-        // If there are as many non-zeros as there are possible positions, throw exception
-        if (nonZeros == size) {
-            throw new NoSuchElementException("There are no empty locations on the board.");
+        // If no zero values found, throw exception
+        if (zeroIndices.isEmpty()) {
+            throw new NoSuchElementException("No spaces on the board are empty.");
         }
 
-        // boolean variable to indicate if a suitable index has been found
-        boolean found = false;
-
-        // Loop through the game board for as many elements are in it
-        int element = 0;
-        while (element < size && !found) {
-            // Generate two random numbers to index into the board
-            int randRow = (int) ((Math.random() * (size - 1)) + 1);
-            int randCol = (int) ((Math.random() * (size - 1)) + 1);
-
-            // If either index contains a non-zero value, do nothing
-
-            element++;
-        }
-        return new int[] {1,1};
+        // Select a random index from zeroIndices
+        Random rand = new Random();
+        int[] randomIndex = zeroIndices.get(rand.nextInt(zeroIndices.size()));
+        return randomIndex;
     }
 
     /**
--- CSDS132/Projects/project-four/src/test/java/projectFour/SlideGameTest.java
+++ CSDS132/Projects/project-four/src/test/java/projectFour/SlideGameTest.java
@@ -1,5 +1,15 @@
 package projectFour;
 
+import java.lang.reflect.Method;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * A comprehensive class and game tester for the 2048 parody called SlideGame.
+ * 
+ * @author Trevor Swan
+ * @version CSDS132 - Spring 2024
+ */
 public class SlideGameTest {
     
 }
