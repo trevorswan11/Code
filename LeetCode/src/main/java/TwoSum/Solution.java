b9b9c18273b1b865fd2b89b87b53808b17b77510
Trevor Swan <tcs94@case.edu>
2024-05-22 01:01:17 -0400

    updated code

--- LeetCode/src/main/java/TwoSum/Solution.java
+++ LeetCode/src/main/java/TwoSum/Solution.java
@@ -25,6 +25,15 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> complements = new HashMap<>();
         
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (complements.containsKey(complement)) {
+                return new int[] { complements.get(complement), i };
+            }
+
+            complements.put(nums[i], i);
+        }
         // Return the original array if nothing was found
         return nums;
     }
