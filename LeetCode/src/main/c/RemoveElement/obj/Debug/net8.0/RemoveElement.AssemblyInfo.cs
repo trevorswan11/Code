9f73c0c88083d85f64e0194858f6a08296d1a9c5
Trevor Swan <tcs94@case.edu>
2024-09-27 17:41:15 -0400

    stack valid paren

--- LeetCode/src/main/c/RemoveElement/obj/Debug/net8.0/RemoveElement.AssemblyInfo.cs
+++ LeetCode/src/main/c/RemoveElement/obj/Debug/net8.0/RemoveElement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RemoveElement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9b3bf263ee52d7028972a75857951983387810")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b353b8f7ad8788ba7b771ddd375a3a169288906")]
 [assembly: System.Reflection.AssemblyProductAttribute("RemoveElement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RemoveElement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
